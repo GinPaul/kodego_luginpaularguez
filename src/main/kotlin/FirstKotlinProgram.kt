26d98de9ee33c9a7d09e190754f8b845a2cf0315
lugin paul arguez <luginlpp@gmail.com>
2022-09-01 20:55:05 +0800

    first commit

---
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -0,0 +1,13 @@
+fun main() {
+    //1st comment
+    //2nd comment
+    //3rd comment to document code
+    /*
+        Author: Lugin Paul Arguez
+        Date Created: 9/1/2022
+        Filename: FirstKotlinProgram.kt
+     */
+    println("Hello")
+    println("Lugin Paul")
+    println("Arguez!")
+}
