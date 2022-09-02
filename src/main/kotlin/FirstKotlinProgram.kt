1f7c2e888e2458744628451cdd507508b92e4a61
lugin paul arguez <luginlpp@gmail.com>
2022-09-02 21:04:21 +0800

    fourth commit

--- src/main/kotlin/FirstKotlinProgram.kt
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -7,9 +7,36 @@ fun main() {
         Date Created: 9/1/2022
         Filename: FirstKotlinProgram.kt
      */
-    println("Hello")
-    println("Lugin Paul")
-    println("Arguez!")
-    println("Hello!!!!!")
-    println("Shaugn Lukas")
+    var firstName : String = "Lugin"
+    firstName = "Paul"
+    var age : Int = 37
+    var grade : Double = 99.00
+    var section : Char = 'A'
+    var isEnrolled : Boolean = true
+
+    println("My Name is $firstName")
+    print("My Age is $grade")
+    println(" my grade is grade")
+    println(section)
+    println(isEnrolled)
+
+    println("Enter your name:")
+    var name : String = readln()
+
+    println("My Name is $name")
+
+    println("Enter your number:")
+    var number : Int = readln().toInt()
+
+    println("My number is $number")
+
+    println("Enter your number 1:")
+    var number1 : Int = readln().toInt()
+
+    println("Enter your number 2:")
+    var number2 : Int = readln().toInt()
+
+    var sum : Int = number1 + number2
+
+    print("The sum is $sum")
 }
