47d5949c00d0216b2995193374ce7a4f85313560
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 16:01:40 +0800

    fifth commit for Activity01-B.kt

--- src/main/kotlin/exercises/Activity01-B.kt
+++ src/main/kotlin/exercises/Activity01-B.kt
@@ -2,7 +2,7 @@ package exercises
 
 fun main(){
     println()
-    var instruction1 : String = "Available items for today:"
+    var instruction1 : String = "Items in this Library:"
     var item1 : String = "Encyclopedia"
     var characteristic1 : String = "will give you introductory information to help you broaden or narrow your topic, while also providing keywords and terms needed to conduct further research."
     var item2 : String = "Magazine"
@@ -18,6 +18,27 @@ fun main(){
     println("Library Item 5: $item5")
 
     println()
+    //availability
+    var itemCopy1 : Int = 5
+    var itemCopy2 : Int = 0
+    var itemCopy3 : Int = 0
+    var itemCopy4 : Int = 0
+    var itemCopy5 : Int = 0
+
+    //status
+    var itemAvail1 : Boolean = itemCopy1 > 0
+    var itemAvail2 : Boolean = itemCopy2 > 0
+    var itemAvail3 : Boolean = itemCopy3 > 0
+    var itemAvail4 : Boolean = itemCopy4 > 0
+    var itemAvail5 : Boolean = itemCopy5 > 0
+
+    println("$item1 is currently available. $itemAvail1")
+    println("$item2 is currently available. $itemAvail2")
+    println("$item3 is currently available. $itemAvail3")
+    println("$item4 is currently available. $itemAvail4")
+    println("$item5 is currently available. $itemAvail5")
+
+    println()
 
     println("Which item you want to borrow?")
     var prompt : String = readln()
@@ -29,4 +50,6 @@ fun main(){
     println()
     println("You have confirmed to borrow $prompt. Please get your slip and proceed to the counter.")
     println()
+
+    //note: selection of items not yet limited to encyclopedia for the moment and answer is limited to Yes. :)
 }
