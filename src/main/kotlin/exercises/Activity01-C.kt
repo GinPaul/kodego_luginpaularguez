5faac4c994ad9297bf66a53587a86b475b84f7ff
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 14:00:55 +0800

    first commit for Activity01-C.kt

---
+++ src/main/kotlin/exercises/Activity01-C.kt
@@ -0,0 +1,18 @@
+package exercises
+
+fun main(){
+    var ligoRedchili : String = "Ligo Red Chili"
+    var centuryTunaflakes : String = "Century Tuna Flakes in Oil"
+    var category1 : String = "Canned Goods"
+    var item3 : String = "Lucky Me Pancit Canton Chili Mansi"
+    var category2 : String = "Noodles"
+    var item4 : String = "Coconut Oil"
+    var category3 : String = "Cooking Oil"
+    var item5 : String = "Bear Brand"
+    var item6 : String = "Alaska"
+    var category4 : String = "Powdered Milk"
+
+    println()
+
+
+}
