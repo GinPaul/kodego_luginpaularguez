93552bbdd6824557923f268f14b91e489f632891
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 13:31:22 +0800

    third commit for Activity01-B.kt

--- src/main/kotlin/exercises/Activity01-B.kt
+++ src/main/kotlin/exercises/Activity01-B.kt
@@ -4,6 +4,7 @@ fun main(){
     println()
     var instruction1 : String = "Available items for today:"
     var item1 : String = "Encyclopedia"
+    var characteristic1 : String = "will give you introductory information to help you broaden or narrow your topic, while also providing keywords and terms needed to conduct further research."
     var item2 : String = "Magazine"
     var item3 : String = "Newspaper"
     var item4 : String = "Audio Book"
@@ -21,6 +22,11 @@ fun main(){
     println("Which item you want to borrow?")
     var prompt : String = readln()
     println()
-    println("Please proceed to the counter and show your slip for the $prompt")
-
+    println("$prompt $characteristic1")
+    println()
+    println("Are you sure you want to borrow this item?")
+    var prompt2 : String = readln()
+    println()
+    println("You have confirmed to borrow $prompt. Please get your slip and proceed to the counter.")
+    println()
 }
