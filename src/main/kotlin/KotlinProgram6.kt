57d64293fc0038b35ff00d9210d1eca94249d3ac
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 16:06:11 +0800

    first commit for KotlinProgram6.kt

---
+++ src/main/kotlin/KotlinProgram6.kt
@@ -0,0 +1,10 @@
+fun main(){
+    //use of loop (no need to type each number)
+    //for(number in 1 .. 10){
+       // println(number)
+   // }
+
+    for(letters in 'B' .. 'X'){
+        println(letters)
+    }
+}
