89e98d7d8fd156c152de032d3124bfab12a968ca
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 16:04:19 +0800

    first commit for KotlinProgram4.kt

--- src/main/kotlin/KotlinProgram4.kt
+++ src/main/kotlin/KotlinProgram4.kt
@@ -5,10 +5,26 @@ fun main(){
         println("Hello")
     }
 
-    var grade : Int = 75
+    var grade : Int = 74
     if(grade >= 75){
         println("You Passed")
+    }else{
+        println("you failed")
     }
+
+    var grade1 : Int = 74
+    if((grade1 >= 96)&&(grade1 <= 100)){
+        println("1.00")
+    }else if((grade1 >=86)&&(grade1 <=95)){
+        println("1.75")
+    }else if ((grade1 >=76)&&(grade1 <=85)){
+        println("2.25")
+    }else if(grade1 < 75) {
+        println("5.0")
+    }else{
+        println("Invalid grade!")
+    }
+
 
     //range
     var idNumber : Int = 1
@@ -17,5 +33,7 @@ fun main(){
         println("valid ID number")
         println("SUCCESS!!")
     }
+
+
     println("Line ends")
 }
