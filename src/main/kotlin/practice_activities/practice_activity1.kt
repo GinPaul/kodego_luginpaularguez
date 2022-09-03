96ca14fe679085cb67fa532f4feba10f1af9ff78
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 22:40:17 +0800

    first commit

---
+++ src/main/kotlin/practice_activities/practice_activity1.kt
@@ -0,0 +1,18 @@
+package practice_activities
+
+fun main(){
+    println()
+
+    println("What is your First Name?")
+    var firstName : String = readln()
+
+    println("What is your Middle Name?/Initial?")
+    var middleName : String = readln()
+
+    println("What is your Last Name?")
+    var lastName : String = readln()
+
+    println()
+    println("Your name is $firstName $middleName $lastName.")
+
+}
