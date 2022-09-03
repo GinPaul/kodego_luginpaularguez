a2e34cd72e317d2c2003adc3771a0756b9716f49
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 15:05:08 +0800

    first commit for KotlinProgram4.kt

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var number : Int = 10
+
+    if(number == 10){
+        println("Hello")
+    }
+
+    var grade : Int = 75
+    if(grade >= 75){
+        println("You Passed")
+    }
+
+    //range
+    var idNumber : Int = 1
+        //true statement      //true statement
+    if((idNumber >= 1)&&(idNumber <= 3)){
+        println("valid ID number")
+        println("SUCCESS!!")
+    }
+    println("Line ends")
+}
