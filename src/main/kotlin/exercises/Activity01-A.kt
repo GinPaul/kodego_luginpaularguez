fe637a29b474643533a160fdf5f80308e97580c1
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 12:39:38 +0800

    second commit for Activity01-A.kt

--- src/main/kotlin/exercises/Activity01-A.kt
+++ src/main/kotlin/exercises/Activity01-A.kt
@@ -2,25 +2,50 @@ package exercises
 
 fun main(){
 
-    var fullName : String = "Lugin Paul Arguez"
-    var age : Int = 37
-    var sex : String = "Male"
-    var address : String = "Caloocan City"
-    var isEnrolled : Boolean = true
-    var section : Char = 'A'
-    var emailAddress: String = "luginlpp@gmail.com"
-    var mobileNumber: Long = 639171234567
-    var currentScore : Double = 99.99
+    var studentNumber : Int = 2022001
+    var studentName1 : String = "Lugin Paul Arguez"
+    var age1 : Int = 37
+    var sex1 : String = "Male"
+    var address1 : String = "Caloocan City"
+    var isEnrolled1 : Boolean = true
+    var section1 : Char = 'A'
+    var emailAddress1 : String = "luginlpp@gmail.com"
+    var mobileNumber1 : Long = 639171234567
+    var currentScore1 : Double = 99.99
 
-    println("Student 001 Name: $fullName")
-    println("Age: $age")
-    println("Sex: $sex")
-    println("Address: $address")
-    println("Is Enrolled?: $isEnrolled")
-    println("Section Name: $section")
-    println("Email Address?: $emailAddress")
-    println("Mobile Number: $mobileNumber")
-    println("Current Score: $currentScore")
+    println("Student Number: $studentNumber")
+    println("Full Name: $studentName1")
+    println("Age: $age1")
+    println("Sex: $sex1")
+    println("Address: $address1")
+    println("Is Enrolled?: $isEnrolled1")
+    println("Section Name: $section1")
+    println("Email Address?: $emailAddress1")
+    println("Mobile Number: $mobileNumber1")
+    println("Current Score: $currentScore1")
 
+    println()
+
+    var studentNumber2 : Int = 2022002
+    var studentName2 : String = "Susan dela Cruz"
+    var age2 : Int = 40
+    var sex2 : String = "Female"
+    var address2 : String = "Quezon City"
+    var isEnrolled2 : Boolean = true
+    var section2 : Char = 'B'
+    var emailAddress2 : String = "s.delacruz@gmail.com"
+    var mobileNumber2 : Long = 639208889999
+    var currentScore2 : Double = 98.00
+
+    println("Student Number: $studentNumber2")
+    println("Student Name: $studentName2")
+    println("Age: $age2")
+    println("Sex: $sex2")
+    println("Address: $address2")
+    println("Is Enrolled?: $isEnrolled2")
+    println("Section Name: $section2")
+    println("Email Address?: $emailAddress2")
+    println("Mobile Number: $mobileNumber2")
+    println("Current Score: $currentScore2")
 
 }
---
+++ src/main/kotlin/exercises/Activity01-B.kt
@@ -0,0 +1,10 @@
+package exercises
+
+fun main(){
+    var item1 : String = "Encyclopedia"
+    var item2 : String = "Magazine"
+    var item3 : String = "Newspaper"
+    var item4 : String = "Audio Book"
+    var item5 : String = "CD"
+
+}
