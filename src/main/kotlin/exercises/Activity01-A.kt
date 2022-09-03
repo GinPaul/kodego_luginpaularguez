efb2d42213856af2b35681e25d1e1c7acb9d18c9
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 11:59:38 +0800

    first commit for Activity01-A.kt

---
+++ src/main/kotlin/exercises/Activity01-A.kt
@@ -0,0 +1,26 @@
+package exercises
+
+fun main(){
+
+    var fullName : String = "Lugin Paul Arguez"
+    var age : Int = 37
+    var sex : String = "Male"
+    var address : String = "Caloocan City"
+    var isEnrolled : Boolean = true
+    var section : Char = 'A'
+    var emailAddress: String = "luginlpp@gmail.com"
+    var mobileNumber: Long = 639171234567
+    var currentScore : Double = 99.99
+
+    println("Student 001 Name: $fullName")
+    println("Age: $age")
+    println("Sex: $sex")
+    println("Address: $address")
+    println("Is Enrolled?: $isEnrolled")
+    println("Section Name: $section")
+    println("Email Address?: $emailAddress")
+    println("Mobile Number: $mobileNumber")
+    println("Current Score: $currentScore")
+
+
+}
