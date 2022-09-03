36031fa553db387ba9299657bbeab70be8cbd29d
lugin paul arguez <luginlpp@gmail.com>
2022-09-03 09:49:46 +0800

    initial commit for KotlinProgram2.kt

---
+++ src/main/kotlin/KotlinProject2.kt
@@ -0,0 +1,22 @@
+fun main (){
+    var string1 : String = "this is my String"
+    var string2 : String = "this is my String too!"
+    //string character - will show what letter is on the 11th space
+    println(string1[11])
+    //string length - will return how many characters are there
+    println(string1.length)
+    println(string2.length)
+    //string equality - will show if string 1 is equal in value to string 2
+    println(string1 == string2)
+    //string uppercase/lowercase will convert lowercase to uppercase and vice versa
+    println(string2.uppercase())
+    //startswith
+    println(string2.startsWith(prefix = "the"))
+    //endswith
+    println(string2.endsWith(suffix = "too!"))
+    //concatenate
+    println(string2.)
+
+    
+
+}
