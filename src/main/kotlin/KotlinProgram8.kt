fc703cd727c068ee738c05d7a036939bddeda73c
lugin paul arguez <luginlpp@gmail.com>
2022-09-05 19:39:17 +0800

    first commit

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,25 @@
+fun main(){
+    //while loop
+    //init
+    //++ is to increment
+
+    var ctr : Int = 1
+
+    ctr++
+    println(ctr)
+
+    //-- ctr is to decrement
+
+    var ctr1 : Int = 1
+    ctr1--
+    println(ctr1)
+
+    //while condition >> won't stop the loop if the value is less than or equal to 5
+    while(ctr <=5){
+        println("Hello World! $ctr")
+        //update
+        ctr++
+    }
+
+
+}
