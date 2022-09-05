2f43e04eae666f655b0151bd2e050faaf6812384
lugin paul arguez <luginlpp@gmail.com>
2022-09-05 19:54:40 +0800

    first commit

---
+++ src/main/kotlin/KotlinProgram9.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    //do while, a variation of while loop using 2 keywords
+    //init
+    var ctr : Int = 5
+    do{
+        println("Hello World!")
+        ctr++ //this is the update
+    }while(ctr <=5) //condition
+
+
+
+}
