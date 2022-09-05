a66cc00ea6356fa8a95e5360192d088882c70f3f
lugin paul arguez <luginlpp@gmail.com>
2022-09-05 17:56:39 +0800

    second commit

--- src/main/kotlin/practice_activities/practice_activity2.kt
+++ src/main/kotlin/practice_activities/practice_activity2.kt
@@ -3,19 +3,19 @@ package practice_activities
 fun main(){
 
     //Enter grades
-    println("Enter grade in Math:")
+    println("Enter Grade in Math:")
     var gradeMath = readln().toDouble()
 
-    println("Enter grade in English:")
+    println("Enter Grade in English:")
     var gradeEnglish = readln().toDouble()
 
-    println("Enter grade in Filipino:")
+    println("Enter Grade in Filipino:")
     var gradeFilipino = readln().toDouble()
 
     //computation
     var sumGrade: Double = (gradeMath + gradeEnglish + gradeFilipino)
     var averageGrade : Double = (sumGrade/3)
 
-    println("Average grade is $averageGrade")
+    println("Average Grade is: $averageGrade")
 
 }
