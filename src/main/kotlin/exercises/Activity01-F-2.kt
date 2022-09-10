7e33c4a7fd31c7562ce5e420b057a597dec007f8
lugin paul arguez <luginlpp@gmail.com>
2022-09-10 14:39:32 +0800

    first commit

---
+++ src/main/kotlin/exercises/Activity01-F-2.kt
@@ -0,0 +1,24 @@
+package exercises
+
+fun main() {
+
+    println("Enter String:")
+    var input: String = readln()
+    palindromeChecker(input)
+
+}
+
+fun palindromeChecker(input:String){
+    var string1 = input.uppercase()
+    var reverse = string1.reversed()
+
+    if (string1 == reverse){
+        println("$input is a palindrome.")
+    }else{
+        println("$input is not a palindrome.")}
+
+
+
+}
+
+
