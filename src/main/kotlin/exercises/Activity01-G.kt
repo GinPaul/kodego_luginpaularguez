52680639dca3ede3aa8e2e31215c20b92ebb2a7b
lugin paul arguez <luginlpp@gmail.com>
2022-09-11 17:31:49 +0800

    first commit Sep 11 2022

---
+++ src/main/kotlin/exercises/Activity01-G.kt
@@ -0,0 +1,54 @@
+package practice_activities
+
+fun main() {
+
+    //first variables
+    var integer1: Int = 0
+    var integer2: Int = 0
+
+    //entry of integers
+    println("Enter the first whole number:")
+    integer1 = readln().toInt()
+
+    println("Enter the second whole number:")
+    integer2 = readln().toInt()
+
+    //Bigger or lower value?
+    if (integer1 > integer2) {
+        println("Bigger value: $integer1, Lower value:$integer2")
+    } else {
+        println("Bigger value: $integer2, Lower value: $integer1")
+    }
+
+    //variable for the prime numbers
+    var prime = mutableListOf<Int>()
+
+    if(isPrimeNumber(integer1) == true){
+        //add to prime mutanle list
+        prime.add(integer1)
+    }else{
+        println("$integer1 is not a prime number.")
+
+    }
+    println()
+
+    if(isPrimeNumber(integer2) == true){
+        //add to prime mutable list
+        prime.add(integer2)
+        println(prime.sortedDescending())
+    }else{
+        println()
+        println("$integer2 is not a prime number.")}
+
+}
+
+//new class: prime number decision
+fun isPrimeNumber(number: Int): Boolean {
+    for (i in 2..number / 2) {
+        if (number % i == 0) {
+        return false}
+        }
+        return true
+    }
+
+
