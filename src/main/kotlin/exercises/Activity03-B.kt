429126a965551ba912c18f0d385c760d6d1f8eb1
lugin paul arguez <luginlpp@gmail.com>
2022-09-25 10:28:42 +0800

    Sep 25 2022 commit

---
+++ src/main/kotlin/exercises/Activity03-B.kt
@@ -0,0 +1,57 @@
+import mu.KotlinLogging
+private val logger = KotlinLogging.logger {}
+
+fun main() {
+
+    var num1: Int = 0
+    try {
+        println("Please enter an amount:")
+        num1 = readln().toInt()
+    } catch (e: NumberFormatException) {
+        println(e)
+        println("Invalid input. Try again.")
+        println()
+    }
+
+    var inputText: String = ""
+    var num2: Int = 0
+    var total: Int = num1 + num2
+
+    //amount entry and operation loop
+    do {
+        println("Do you want to add an amount?")
+        println("Please enter Y for Yes and N for No:")
+        inputText = readln().uppercase()
+
+        if (inputText == "Y") {
+            do {
+                try {
+                    println("Enter the amount:")
+                    num2 = readln().toInt()
+                    total += num2
+                } catch (e: NumberFormatException) {
+                    println(e)
+                    println("Invalid input. Try again.")
+                    println()
+                }
+            } while (false)
+            println("The amount you entered is $num2")
+            println("The total amount you entered is $total.")
+
+        } else if (inputText == "N") {
+            println("The total amount you entered is $total")
+
+            println("Divide the total amount by how many?")
+            var divisor: Int = readln().toInt()
+
+            var finalAmount: Int = total / divisor
+            println("The resulting amount is $finalAmount")
+            break
+        } else
+            println("Invalid input. Please try again.")
+
+    } while (inputText !== "N")
+
+    println()
+    println("Thank you for using the service.")
+}
