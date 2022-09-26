f420204ec2715ab16eed9d7c5a34d07ec599111a
lugin paul arguez <luginlpp@gmail.com>
2022-09-26 22:20:54 +0800

    Sep 26 2022 commit

--- src/main/kotlin/exercises/Activity01-I.kt
+++ src/main/kotlin/exercises/Activity01-I.kt
@@ -15,17 +15,7 @@ var freshVegInventory = mutableListOf(4,5,6)
 var nonFood = mutableListOf("Surf Sunfresh Powder", "Zonrox Bleach Original", "Downy Antibac")
 var nonFoodInventory = mutableListOf(10,20,50)
 
-fun main(){
-
-
-//    var noodles = mutableMapOf("Lucky Me Beef" to 3, "Lucky Me Pancit Canton" to 5, "Nissin Cup Noodles" to 10)
-//    var noodlesInventory = mutableListOf(3,5,9)
-//    var freshVegetables = mutableListOf("Alugbati", "Okra", "Squash")
-//    var freshVegInventory = mutableListOf(4,5,6)
-//    var nonFood = mutableListOf("Surf Sunfresh Powder", "Zonrox Bleach Original", "Downy Antibac")
-//    var nonFoodInventory = mutableListOf(10,20,50)
-
-
+fun main() {
 
     println("Welcome to the grocery store!")
     println()
@@ -35,64 +25,72 @@ fun main(){
     println("[3] Update the inventory count")
     println("[3] Exit")
 
-    println("Please select an option:")
-    optionSelection = readln().toInt()
+    do {
+        println("Please select an option:")
+        optionSelection = readln().toInt()
 
-    if (optionSelection == 1){
-        println("Noodles:")
-        println ("$noodles")
+        if (optionSelection == 1) {
+            println("Noodles:")
+            println("$noodles")
 
-        println("Fresh Vegetables:")
-        println("$freshVegetables")
+            println("Fresh Vegetables:")
+            println("$freshVegetables")
 
-        println("Non-food items:")
-        println("$nonFood")
-    }else if (optionSelection == 2){
-        println("Please choose which category you want to add inventory:")
-        println("[1] Noodles")
-        println("[2] Fresh Vegetables")
-        println("[3] Non Food Items")
-        categorySelect = readln().toInt()
-        
-        if (categorySelect == 1){
-            println("Running inventory:")
-            println(noodles)
-            println("Please type the name of the item that you want to add to inventory:")
-            itemNameEntry = readln()
-            println("Please enter a number in pieces:")
-            itemQtyEntry = readln().toInt()
-            noodles[itemNameEntry] = itemQtyEntry
+            println("Non-food items:")
+            println("$nonFood")
+        } else if (optionSelection == 2) {
+            println("Please choose which category you want to add inventory:")
+            println("[1] Noodles")
+            println("[2] Fresh Vegetables")
+            println("[3] Non Food Items")
+            categorySelect = readln().toInt()
+
+            if (categorySelect == 1) {
+                println("Running inventory:")
+                println(noodles)
+                println("Please type the name of the item that you want to add to inventory:")
+                itemNameEntry = readln()
+                println("Please enter a number in pieces:")
+                itemQtyEntry = readln().toInt()
+                noodles[itemNameEntry] = itemQtyEntry
 
 
-            println("The item $itemNameEntry with quantity $itemQtyEntry has been added to the inventory.")
-            println("The updated inventory of is $noodles.")
-            
-        }else if (categorySelect == 2){
-            println(freshVegetables)
-        }else if (categorySelect == 3){
-            println(nonFood)
-        }else
-            println("Invalid entry. Try again.")
-        
-    }else if(optionSelection == 3){
-        println()
-        println("Please choose which category you want to add inventory:")
-        println("[1] Noodles")
-        println("[2] Fresh Vegetables")
-        println("[3] Non Food Items")
-        categorySelectForInvUpdate = readln().toInt()
+                println("The item $itemNameEntry with quantity $itemQtyEntry has been added to the inventory.")
+                println("The updated inventory of is $noodles.")
 
-        if (categorySelectForInvUpdate == 1){
-            println("Please type the name of the item to update:")
-            itemNameToUpdateNoodles = readln()
-            println("Please input the updated quantity:")
-            itemQuantityUpdateNoodles = readln().toInt()
-            noodles[itemNameToUpdateNoodles] = itemQuantityUpdateNoodles
+            } else if (categorySelect == 2) {
+                println(freshVegetables)
+            } else if (categorySelect == 3) {
+                println(nonFood)
+            } else
+                println("Invalid entry. Try again.")
 
-            println("The item $itemNameToUpdateNoodles with quantity $itemQuantityUpdateNoodles has been added to the inventory.")
-            println("The updated inventory of is $noodles.")
+        } else if (optionSelection == 3) {
+            println()
+            println("Please choose which category you want to add inventory:")
+            println("[1] Noodles")
+            println("[2] Fresh Vegetables")
+            println("[3] Non Food Items")
+            categorySelectForInvUpdate = readln().toInt()
+
+            if (categorySelectForInvUpdate == 1) {
+                println("Please type the name of the item to update:")
+                itemNameToUpdateNoodles = readln()
+                println("Please input the updated quantity:")
+                itemQuantityUpdateNoodles = readln().toInt()
+                noodles[itemNameToUpdateNoodles] = itemQuantityUpdateNoodles
+
+                println("The item $itemNameToUpdateNoodles with quantity $itemQuantityUpdateNoodles has been added to the inventory.")
+                println("The updated inventory of is $noodles.")
+            } else if (categorySelectForInvUpdate == 2) {
+                println()
+            }
 
         }
-    }
 
+    }while (optionSelection !== 1 && optionSelection !== 2 && optionSelection !== 3 && optionSelection !== 4)
 }
+
+
+
+
